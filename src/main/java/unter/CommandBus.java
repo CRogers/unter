dba4ea0e98370e5b46c4db14193e50238bbf7d84
Callum Rogers <callumrogers@hotmail.co.uk>
2017-06-18 23:22:51 +0100

    Finish command bus

--- src/main/java/unter/CommandBus.java
+++ src/main/java/unter/CommandBus.java
@@ -1,19 +1,28 @@
 package unter;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import unter.commands.Command;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class CommandBus {
-    private List<Consumer> handlers = new ArrayList<>();
+    private final Multimap<Class, Consumer> handlers = Multimaps.newListMultimap(
+            Maps.newHashMap(),
+            ArrayList::new
+    );
 
     public void submit(Command command) {
-        handlers.forEach(handler -> handler.accept(command));
+        handlers.forEach((commandClass, handler) -> {
+            if (commandClass.isAssignableFrom(command.getClass())) {
+                handler.accept(command);
+            }
+        });
     }
 
     public <T extends Command> void listenFor(Class<T> commandClass, Consumer<T> handler) {
-        handlers.add(handler);
+        handlers.put(commandClass, handler);
     }
 }
--- src/test/java/unter/CommandBusShould.java
+++ src/test/java/unter/CommandBusShould.java
@@ -1,5 +1,6 @@
 package unter;
 
+import org.junit.After;
 import org.junit.Test;
 import unter.commands.Command;
 
@@ -7,15 +8,27 @@ import java.util.function.Consumer;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class CommandBusShould {
     private final CommandBus commandBus = new CommandBus();
 
     private final Consumer<FooCommand> fooCommandHandler1 = mock(Consumer.class, "fooHandler1");
     private final Consumer<FooCommand> fooCommandHandler2 = mock(Consumer.class, "fooHandler2");
+    private final Consumer<BarCommand> barCommandHandler = mock(Consumer.class, "barHandler");
     private final FooCommand fooCommand = mock(FooCommand.class, "fooCommand");
 
     interface FooCommand extends Command {}
+    interface BarCommand extends Command {}
+
+    @After
+    public void after() {
+        verifyNoMoreInteractions(
+                fooCommandHandler1,
+                fooCommandHandler2,
+                barCommandHandler
+        );
+    }
 
     @Test
     public void not_error_when_a_command_is_submitted_but_no_handlers_have_been_registered() {
@@ -38,5 +51,11 @@ public class CommandBusShould {
 
         verify(fooCommandHandler1).accept(fooCommand);
         verify(fooCommandHandler2).accept(fooCommand);
+    }
+
+    @Test
+    public void not_call_handler_of_differing_type_to_command() {
+        commandBus.listenFor(BarCommand.class, barCommandHandler);
+        commandBus.submit(fooCommand);
     }
 }
