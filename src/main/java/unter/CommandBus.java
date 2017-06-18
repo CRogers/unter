b0bb5d11479e45af1034a47a91bee7e08f7bda70
Callum Rogers <callumrogers@hotmail.co.uk>
2017-06-18 23:15:34 +0100

    Test: Not crash with no handler

--- src/main/java/unter/CommandBus.java
+++ src/main/java/unter/CommandBus.java
@@ -2,16 +2,17 @@ package unter;
 
 import unter.commands.Command;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class CommandBus {
-    private Consumer handler = null;
+    private Optional<Consumer> handler = Optional.empty();
 
     public void submit(Command command) {
-        handler.accept(command);
+        handler.ifPresent(handler -> handler.accept(command));
     }
 
     public <T extends Command> void listenFor(Class<T> commandClass, Consumer<T> handler) {
-        this.handler = handler;
+        this.handler = Optional.of(handler);
     }
 }
--- src/test/java/unter/CommandBusShould.java
+++ src/test/java/unter/CommandBusShould.java
@@ -11,16 +11,32 @@ import static org.mockito.Mockito.verify;
 public class CommandBusShould {
     private final CommandBus commandBus = new CommandBus();
 
+    private final Consumer<FooCommand> fooCommandHandler1 = mock(Consumer.class, "fooHandler1");
+    private final Consumer<FooCommand> fooCommandHandler2 = mock(Consumer.class, "fooHandler2");
+    private final FooCommand fooCommand = mock(FooCommand.class, "fooCommand");
+
     interface FooCommand extends Command {}
 
     @Test
-    public void call_command_handler_when_command_of_the_correct_type_is_registered() {
-        Consumer<FooCommand> fooCommandHandler = mock(Consumer.class);
-        commandBus.listenFor(FooCommand.class, fooCommandHandler);
+    public void not_error_when_a_command_is_submitted_but_no_handlers_have_been_registered() {
+        commandBus.submit(fooCommand);
+    }
 
-        FooCommand fooCommand = mock(FooCommand.class);
+    @Test
+    public void call_command_handler_when_command_of_the_correct_type_is_submitted() {
+        commandBus.listenFor(FooCommand.class, fooCommandHandler1);
         commandBus.submit(fooCommand);
 
-        verify(fooCommandHandler).accept(fooCommand);
+        verify(fooCommandHandler1).accept(fooCommand);
+    }
+
+    @Test
+    public void call_all_command_handlers_when_command_of_the_correct_type_is_submitted() {
+        commandBus.listenFor(FooCommand.class, fooCommandHandler1);
+        commandBus.listenFor(FooCommand.class, fooCommandHandler2);
+        commandBus.submit(fooCommand);
+
+        verify(fooCommandHandler1).accept(fooCommand);
+        verify(fooCommandHandler2).accept(fooCommand);
     }
 }
